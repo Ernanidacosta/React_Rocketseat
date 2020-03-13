c09e1ed68ee1bdbda8e0bdde91e0e8e487d6d81c
Ernani da Costa <ernanidacosta@gmail.com>
2020-03-13 13:46:38 -0300

    create component Header

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Curso de React</h1>      
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+     <Header />      
+  </div>
+);
 
 export default App;
